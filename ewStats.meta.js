fe0b3cd00ae15583fede6784e8c0d5ab9630da56
ChrisRaven <kr.kruk@gmail.com>
2017-08-10 13:32:57 +0200

    Update ewStats.meta.js

--- ewStats.meta.js
+++ ewStats.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EyeWire Statistics
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Shows daily, weekly and monthly statistics for EyeWire
 // @author       Krzysztof Kruk
 // @match        https://eyewire.org/
