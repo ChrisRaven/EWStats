b3082f81ba824ce849bc3cc5732d42db823fcab5
Krzysztof Kruk <kr.kruk@gmail.com>
2018-01-11 01:27:29 +0100

    Custom Highlight moved to a separate script

--- ewStats.meta.js
+++ ewStats.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EyeWire Statistics
 // @namespace    http://tampermonkey.net/
-// @version      2.4.1
+// @version      2.5
 // @description  Shows daily, weekly and monthly statistics for EyeWire
 // @author       Krzysztof Kruk
 // @match        https://*.eyewire.org/
